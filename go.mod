e36c2ed2f2b00087673b7b071d5af464311598fe
raul <raulandrei2017@yahoo.com>
2025-04-06 01:36:54 +0300

    chore: add TODOs for manager

--- go.mod
+++ go.mod
@@ -3,6 +3,7 @@ module BruteForce_SearchEnginer
 go 1.23.2
 
 require (
+	github.com/go-chi/chi/v5 v5.2.1 // indirect
 	go.uber.org/multierr v1.11.0 // indirect
 	go.uber.org/zap v1.27.0 // indirect
 )
---
+++ manager/cmd/api/main.go
@@ -0,0 +1,13 @@
+package main
+
+func main() {
+
+	// TODO() create the directory pool
+
+	// TODO() create the results pool
+
+	// TODO() create the endpoints for start, stop, directory-pool, results-pool, get-results
+
+	// TODO() create the process of creating other workers
+
+}

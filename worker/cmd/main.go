40b21b190e265e67832fd7e6e94adb386ea76592
raul <raulandrei2017@yahoo.com>
2025-04-06 00:17:32 +0300

    chore: setup TODOs for flow of worker

--- worker/cmd/main.go
+++ worker/cmd/main.go
@@ -1,11 +1,33 @@
 package main
 
+import (
+	"BruteForce_SearchEnginer/common/model"
+	"BruteForce_SearchEnginer/worker/internal/matcher"
+	"BruteForce_SearchEnginer/worker/internal/repo"
+	"go.uber.org/zap"
+)
+
 type worker struct {
+	logger         *zap.Logger
+	fileRepo       repo.FileRepo
+	requestMatcher matcher.RequestMatcher
+	typeMap        model.FileTypesConfig
+	config         workerConfig
+}
+
+type workerConfig struct {
+	managerURL            string
+	workerStopEndpoint    string
+	directoryPoolEndpoint string
+	resultPoolEndpoint    string
 }
 
 func main() {
 	// TODO() setup logger, repo, matcher, fileTypesConfig
 
-	// TODO() setup
+	// TODO() lookup pool directories
+
+	// stop
+	// TODO() send stop signal to the manager
 
 }

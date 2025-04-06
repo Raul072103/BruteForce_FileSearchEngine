176678a79a1eb9781b32bbde3d97b472d715dc35
raul <raulandrei2017@yahoo.com>
2025-04-06 22:13:39 +0300

    chore: remove 3 TODOs

--- manager/cmd/api/main.go
+++ manager/cmd/api/main.go
@@ -45,12 +45,6 @@ func main() {
 		app.logger.Fatal("server error", zap.Error(err))
 	}
 
-	// TODO() create the directory pool
-
-	// TODO() create the results pool
-
-	// TODO() create the endpoints for start, stop, directory-pool, results-pool, get-results
-
 	// TODO() create the process of creating other workers
 }
 

95c45c50454d4a6c66b94c2ae5e668a504ab350a
raul <raulandrei2017@yahoo.com>
2025-04-06 19:29:19 +0300

    feat(server_config): finish basic manager server configuration

--- manager/cmd/api/main.go
+++ manager/cmd/api/main.go
@@ -1,6 +1,47 @@
 package main
 
+import (
+	"BruteForce_SearchEnginer/common/logger"
+	"BruteForce_SearchEnginer/manager/internal/pool"
+	"expvar"
+	"go.uber.org/zap"
+	"runtime"
+)
+
+const (
+	version = "0.0.0"
+)
+
+type application struct {
+	directoryPool *pool.DirectoryPool
+	resultPool    *pool.ResultPool
+	config        config
+	logger        *zap.Logger
+}
+
+type config struct {
+	addr   string
+	apiURL string
+}
+
 func main() {
+
+	// Performance metrics
+	expvar.NewString("version").Set(version)
+	expvar.Publish("goroutines", expvar.Func(func() any {
+		return runtime.NumGoroutine()
+	}))
+
+	// manager entry point setup
+	app := setup()
+
+	mux := app.mount()
+	app.logger.Fatal("server error", zap.Error(app.run(mux)))
+
+	err := app.run(mux)
+	if err != nil {
+		app.logger.Fatal("server error", zap.Error(err))
+	}
 
 	// TODO() create the directory pool
 
@@ -9,4 +50,24 @@ func main() {
 	// TODO() create the endpoints for start, stop, directory-pool, results-pool, get-results
 
 	// TODO() create the process of creating other workers
+}
+
+func setup() *application {
+	var app application
+
+	dirPool := pool.DirectoryPool{}
+	resultPool := pool.ResultPool{}
+	appLogger := logger.InitLogger("./../../manager.log")
+
+	config := config{
+		addr:   ":8080",
+		apiURL: "localhost:8080",
+	}
+
+	app.directoryPool = &dirPool
+	app.resultPool = &resultPool
+	app.logger = appLogger
+	app.config = config
+
+	return &app
 }
---
+++ manager/cmd/api/search.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"BruteForce_SearchEnginer/common/json"
+	"log"
+	"net/http"
+)
+
+func (app *application) searchHandler(w http.ResponseWriter, r *http.Request) {
+	data := map[string]string{
+		"status":  "ok",
+		"version": version,
+	}
+
+	if err := json.WriteJSON(w, http.StatusOK, data); err != nil {
+		err := json.WriteJSONError(w, http.StatusInternalServerError, err.Error())
+		if err != nil {
+			log.Println("Failed writing to JSON error")
+		}
+	}
+}
